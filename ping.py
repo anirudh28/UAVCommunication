75b2bc57c87434f401c08c3ac4aa408dedd5e5da
anirudh28 <37078265+anirudh28@users.noreply.github.com>
2019-07-01 15:26:51 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -10,12 +10,41 @@
 import os
 import socket
 import sys
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Create a TCP/IP socket
-server_ip = raw_input(‘Enter server IP : ‘)
-while(true)
-	rep = os.system(‘ping ‘ + server_ip)
+import time
+port = 2000            # Reserve a port for your service.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
+host = "192.168.43.79"
+ 
+rep = 0
+##s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Create a TCP/IP socket
+server_ip = raw_input('Enter server IP ')
+while(True):
+	#time.sleep(10)
+	rep = os.system('ping -c 1 '+server_ip)
 	if rep == 0:
-		print ‘server is up’
+		print 'server is up'
+		s.bind((host, port))           
+		s.listen(5)         
+		print 'Server listening....' # Now wait for client connection.
+		while True:
+			conn,addr = s.accept()
+			print 'Got connection from', addr
+			data = conn.recv(1024)
+			print ('server recieved',repr(data))
+
+			filename = 'mytext.txt'
+			f = open(filename,'rb')
+			l = f.read(1024)
+			while(l):
+				conn.send(l)
+				print('Sent ',repr(l))
+				l = f.read(1024)
+			f.close()
+
+			print('Done Sending')
+			conn.send('Thank You for connecting')
+			conn.close()
 	else:
-		print ‘server is down’
+		print 'server is down'
+
 		break
